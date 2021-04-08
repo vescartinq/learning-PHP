c6e2a88348c56fb2c49034ea0577cd7d158fb608
Starkin28 <victoreq00@gmail.com>
2021-04-08 19:24:24 +0200

    variables y concatenar

--- BLUUWEB/variables.php
+++ BLUUWEB/variables.php
@@ -3,10 +3,13 @@
 
 $edad=29;
 $Edad=30; //detecta mayúsculas, variable distinta
+$vivo= false;
 
 $nombre='Victor';
 
 // Concatenar con . (punto)
-echo $nombre.' tiene una edad de '.$edad.' años';
+echo $nombre.' tiene una edad de '.$edad.' años <br/>';
+echo "$nombre tiene una edad de $edad <br/>";
+echo '$nombre tiene una edad de $edad <br/>';
 
 ?>
